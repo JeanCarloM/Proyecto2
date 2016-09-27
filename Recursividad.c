9b906beb679adfec125f05b2622c78de0fabaded
JeanCarloM <toysiito@gmail.com>
2016-09-26 20:17:48 -0500

    Update Recursividad.c
    Para llamar una función recursiva

--- Recursividad.c
+++ Recursividad.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+
+void recursiva(int n);
+
+int main(){
+	recursiva(10);
+}
+
+void recursiva(int n){
+	if(n>0){
+		printf("%d\n", n);
+		recursiva(n-1);		
+	}
+	printf("fin de la funcion\n");
+	return;
+}
